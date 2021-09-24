ce19804ecc33454363b66ca07569a5a5755f0ae7
Christian Roldan Jimenez <60475256+CAZJIMENEZ@users.noreply.github.com>
2021-09-24 16:55:17 -0500

    Add files via upload

---
+++ 22sep/code.js
@@ -0,0 +1,11 @@
+let hola = document.querySelector("h1")
+
+hola.textContent = " perris + perros"
+
+let hol = document.getElementById("text")
+
+hol.textContent = "5+2 " 
+
+var amigos = ["fabio" , "Francisco" , "erick"];
+document.write (amigos[0]);
+
